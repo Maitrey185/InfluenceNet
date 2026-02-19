766dff9bff84289dfe3e357765a8a2801695ea60
Maitrey185 <maitreypatil185@gmail.com>
2026-02-19 14:41:37 +0530

    unwanted exception module removed

--- src/main/java/com/project/InfluenceNet/influencer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/InfluenceNet/influencer/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-//@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InfluencerNotFoundException.class)

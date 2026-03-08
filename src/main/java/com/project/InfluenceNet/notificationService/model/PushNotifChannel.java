cdffbd71cb090e382fae82238abef4b33c8272f8
Maitrey185 <maitreypatil185@gmail.com>
2026-03-09 00:20:08 +0530

    Notif multithread async executor

--- src/main/java/com/project/InfluenceNet/notificationService/model/PushNotifChannel.java
+++ src/main/java/com/project/InfluenceNet/notificationService/model/PushNotifChannel.java
@@ -1,9 +1,12 @@
 package com.project.InfluenceNet.notificationService.model;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PushNotifChannel implements NotificationChannel{
+
+    @Async("notificationExecutor")
     @Override
     public void send(NotificationRequest notificationRequest) {
         System.out.println("Push");

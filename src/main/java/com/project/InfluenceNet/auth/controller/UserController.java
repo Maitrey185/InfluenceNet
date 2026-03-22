4de63309c1c102a5c1d33bba20fc28929c454402
Maitrey185 <maitreypatil185@gmail.com>
2026-03-22 22:05:01 +0530

    User refactored to common contract

--- src/main/java/com/project/InfluenceNet/auth/controller/UserController.java
+++ src/main/java/com/project/InfluenceNet/auth/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.project.InfluenceNet.auth.controller;
 
-import com.project.InfluenceNet.auth.entity.User;
-import com.project.InfluenceNet.auth.repository.UserRepository;
+import com.project.InfluenceNet.contracts.InfluencerPostContract.User;
 import com.project.InfluenceNet.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
@@ -24,5 +25,10 @@ public class UserController {
     @GetMapping("/getByUserName/{username}")
     public ResponseEntity<User> getByUserName(@PathVariable String username){
         return ResponseEntity.ok(authService.loadUserByUsername(username));
+    }
+
+    @GetMapping("/getById/{userId}")
+    public ResponseEntity<User> getById(@PathVariable UUID userId){
+        return ResponseEntity.ok(authService.findById(userId));
     }
 }
